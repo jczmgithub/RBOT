8aec73ccf607574073fb0ad96e88eaca0c306d31
xenlord <alopezca16dw@ikzubirimanteo.com>
2017-10-20 08:27:38 +0200

    Logo metido

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,12 +1,10 @@
 <nav class="navbar navbar-default">
 
-        <div class="navbar-header">
-            <a class="navbar-brand" href="#">
-                <img id="logo" src="/img/domotek-logo.png">
-            </a>
-        </div>
     <div class="container-fluid">
         <div class="navbar-header">
+            <a class="navbar-brand" href="/">
+                <img class="icon-bar" id="logo" src="/img/domotek-logo.png">
+            </a>
             <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
@@ -15,11 +13,13 @@
 
         </div>
         <div class="collapse navbar-collapse" id="myNavbar">
+
+
             <ul class="nav navbar-nav">
-                <li class="active"><a href="#">Home</a></li>
+                <li class="active"><a href="/">Home</a></li>
                 <li><a href="#">Page 1</a></li>
                 <li><a href="#">Page 2</a></li>
-                <li><a href="#">Page 3</a></li>
+                <li><a href="contacto">Contacto</a></li>
             </ul>
             <ul class="nav navbar-nav navbar-right">
                 <li><a data-toggle="modal" data-target="#entrar"><span class="glyphicon glyphicon-user" data-toggle="modal" data-target="#entrar"></span>Entrar</a></li>
