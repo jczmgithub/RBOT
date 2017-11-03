5830601a6bb62f4e9e3a511b08e70963b9dce3d9
xenlord <alopezca16dw@ikzubirimanteo.com>
2017-11-03 08:26:59 +0100

    Setup 1.0

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -35,3 +35,10 @@
         </div>
     </div>
 </nav>
+<script>
+    $('ul.nav li.dropdown').hover(function() {
+        $(this).find('.dropdown-menu').stop(true, true).delay(20).fadeIn(500);
+    }, function() {
+        $(this).find('.dropdown-menu').stop(true, true).delay(20).fadeOut(500);
+    });
+</script>
