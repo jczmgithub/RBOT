a95208b05ceebd64d170423c529591fcd55b69a1
“Juan” <jcasinoste16dw@ikzubirimanteo.com>
2017-11-30 19:59:27 +0100

    pruebas estructura carpetas home

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -11,54 +11,52 @@
 
     <div class="container">
         <div id="infos">
-        <!-- Example row of columns -->
+            <!-- Example row of columns -->
             <h1 id="titles" class="text-center">Control del robot</h1>
-        <div class="row text-center">
-            <div class="col-md-4">
-                <div class="panel panel-default">
-                    <div class="panel-heading">
-                        <h3>RBX1</h3>
+            <div class="row text-center">
+                <div class="col-md-4">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <h3>RBX1</h3>
+                        </div>
+                        <div class="panel-body">
+                            El RBX1 (también conocido como Remix) es un brazo de robot impreso de 6 ejes 3D. El robot se construye utilizando...</p>
+                            <p class="caption"><a class="btn btn-rbt btn-lg btn-block" href="RBX1" role="button">Ver Robot &raquo;</a></p>
+                        </div>
                     </div>
-                    <div class="panel-body">
-                        El RBX1 (también conocido como Remix) es un brazo de robot impreso de 6 ejes 3D. El robot se construye utilizando...</p>
-                        <p class="caption"><a class="btn btn-rbt btn-lg btn-block" href="RBX1" role="button">Ver Robot &raquo;</a></p>
+                </div>
+
+                <div class="col-md-4 ">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <h3>Control por funciones</h3>
+                        </div>
+                        <div class="panel-body">
+                            El brazo robótico puede ser controlado por motores. Mueve los ejes y crea tu secuencia de movimientos.<p/>
+                            <p class="caption"><a class="btn btn-rbt btn-lg btn-block" href="error404" role="button">Detalles <span class="glyphicon glyphicon-console"></span></a></p>
+                        </div>
                     </div>
+                </div>
+
+                <div class="col-md-4">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <h3>Control manual</h3>
+                        </div>
+                        <div class="panel-body">
+                    El brazo robótico también se puede controlar mediante un mando de consola. ¡Un mando Xbox para controlar motores!<p/>
+                    <p class="caption"><a class="btn btn-rbt btn-lg btn-block" href="error404" role="button">Mando <i class="fa fa-gamepad" aria-hidden="true"></i></a></p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
 
-        <div class="col-md-4 ">
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                    <h3>Control por funciones</h3>
-                </div>
-                <div class="panel-body">
-                    El brazo robótico puede ser controlado por motores. Mueve los ejes y crea tu secuencia de movimientos.<p/>
-                    <p class="caption"><a class="btn btn-rbt btn-lg btn-block" href="error404" role="button">Detalles <span class="glyphicon glyphicon-console"></span></a></p>
-                </div>
-            </div>
-        </div>
-            <div class="col-md-4">
-                <div class="panel panel-default">
-                    <div class="panel-heading">
-                        <h3>Control manual</h3>
-                    </div>
-                    <div class="panel-body">
-                El brazo robótico también se puede controlar mediante un mando de consola. ¡Un mando Xbox para controlar motores!<p/>
-                <p class="caption"><a class="btn btn-rbt btn-lg btn-block" href="error404" role="button">Mando <i class="fa fa-gamepad" aria-hidden="true"></i></a></p>
-                    </div>
-                </div>
-        </div>
-        </div>
-        </div>
         <div id="videoSeccion">
-        <div id=videoYoutube class="embed-responsive embed-responsive-16by9 text-center" >
-            <iframe width="1024" height="480" src="https://www.youtube.com/embed/QTmILKjmvlk?rel=0&amp;start=160" frameborder="0" allowfullscreen></iframe>
-        </div>
+            <div id=videoYoutube class="embed-responsive embed-responsive-16by9 text-center" >
+                <iframe width="1024" height="480" src="https://www.youtube.com/embed/QTmILKjmvlk?rel=0&amp;start=160" frameborder="0" allowfullscreen></iframe>
+            </div>
         </div>
     </div>
-    <br/>
-    </div>
-
-
 
 @stop
