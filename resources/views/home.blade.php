07c1540402492248d5f8e4f05da6bd881a92a77f
batlupus <lopez05k@gmail.com>
2017-12-11 14:15:23 +0100

    Cambios landing page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.default')
 
 @section('huecoNav')
-    <li ><a href="#seccionbrazo">Características</a></li>
+    <li ><a href="#seccionbrazoFIX">Características</a></li>
     <li ><a href="#servicios">Servicios</a></li>
     <li ><a href="#contacto">Contacto</a></li>
 @stop
@@ -9,12 +9,10 @@
 @section('home')
 
     <div class="container-fluid text-center" id="inicio">
-        <div id="descripcion">
-            <h1>eivneesnse</h1>
-        </div>
+            <h1 id="descripcion">
+                eivneesnse
+            </h1>
     </div>
-
-
     <div id="seccionbrazo" class="container-fluid text-center">
         <div class="row">
             <div class="col-lg-6">
@@ -32,9 +30,7 @@
                 <p>¡Disfruta de nustra aplicación web!</p>
 
             </div>
-            <div class="col-lg-6">
-                <img style="margin: auto" src="/img/rbx4.png" class="img-responsive" alt="Mano robot">
-            </div>
+                <img style="position: center  " src="/img/rbx4.png" class="img-responsive col-lg-4 " alt="Mano robot">
         </div>
 
     </div>
@@ -74,63 +70,106 @@
         </div>
     </div>
 
+    {{--<div id="contenedorContacto">--}}
+        {{--<div class="contenido container">--}}
+            {{--<div class="row" id="contacto">--}}
+                {{--<div class="col-md-8 col-md-offset-2">--}}
+                    {{--<h2 class="text-center">CONTACTO: </h2>--}}
+                            {{--@if ($errors->any())--}}
+                                {{--<div class="alert alert-danger">--}}
+                                    {{--<ul>--}}
+                                        {{--@foreach ($errors->all() as $error)--}}
+                                            {{--<div class="alert alert-danger">--}}
+                                                {{--<strong>Error! </strong> {{ $error }}--}}
+                                            {{--</div>--}}
+                                        {{--@endforeach--}}
+                                    {{--</ul>--}}
+                                {{--</div>--}}
+                            {{--@endif--}}
+                            {{--@if (session('status'))--}}
+                                {{--<div class="alert alert-success">--}}
+                                    {{--{{ session('status') }}--}}
+                                {{--</div>--}}
+                            {{--@endif--}}
+                            {{--<form class="form-horizontal" method="POST" action="contacto">--}}
+                                {{--{{ csrf_field() }}--}}
+
+                                {{--<div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">--}}
+                                    {{--<label class="control-label col-md-4" for="nombre">Nombre:</label>--}}
+                                    {{--<div class="col-md-6">--}}
+                                        {{--<input type="text" class="form-control" id="nombre" placeholder="Introduce tu nombre" name="nombre">--}}
+                                    {{--</div>--}}
+                                {{--</div>--}}
+
+                                {{--<div class="form-group">--}}
+                                    {{--<label class="control-label col-md-4" for="email">Email:</label>--}}
+                                    {{--<div class="col-sm-6">--}}
+                                        {{--<input type="email" class="form-control" id="email" placeholder="Introduce email" name="email">--}}
+                                    {{--</div>--}}
+                                {{--</div>--}}
+                                {{--<div class="form-group">--}}
+                                    {{--<label class="control-label col-md-4" for="mensaje">Contenido:</label>--}}
+                                    {{--<div class="col-sm-6">--}}
+                                        {{--<textarea class="form-control" rows="3" id="mensaje" name="mensaje" placeholder="Contenido del mensaje"></textarea>--}}
+                                    {{--</div>--}}
+                                {{--</div>--}}
+                                {{--<div class="form-group">--}}
+                                    {{--<div class="col-md-6 col-md-offset-4">--}}
+                                        {{--<button type="submit" class="btn btn-rbt">Enviar</button>--}}
+                                    {{--</div>--}}
+                                {{--</div>--}}
+                            {{--</form>--}}
+                        {{--</div>--}}
+            {{--</div>--}}
+        {{--</div>--}}
+    {{--</div>--}}
+
+
     <div id="contenedorContacto">
-        <div class="contenido container">
-            <div class="row" id="contacto">
-                <div class="col-md-8 col-md-offset-2">
-                    <div class="panel panel-default">
-                        <div class="panel-heading"><h3>Contacto</h3></div>
-                        <div class="panel-body">
-                            @if ($errors->any())
-                                <div class="alert alert-danger">
-                                    <ul>
-                                        @foreach ($errors->all() as $error)
-                                            <div class="alert alert-danger">
-                                                <strong>Error! </strong> {{ $error }}
-                                            </div>
-                                        @endforeach
-                                    </ul>
-                                </div>
-                            @endif
-                            @if (session('status'))
-                                <div class="alert alert-success">
-                                    {{ session('status') }}
-                                </div>
-                            @endif
-                            <form class="form-horizontal" method="POST" action="contacto">
-                                {{ csrf_field() }}
-
-                                <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                                    <label class="control-label col-md-4" for="nombre">Nombre:</label>
-                                    <div class="col-md-6">
-                                        <input type="text" class="form-control" id="nombre" placeholder="Introduce tu nombre" name="nombre">
-                                    </div>
-                                </div>
-
-                                <div class="form-group">
-                                    <label class="control-label col-md-4" for="email">Email:</label>
-                                    <div class="col-sm-6">
-                                        <input type="email" class="form-control" id="email" placeholder="Introduce email" name="email">
-                                    </div>
-                                </div>
-                                <div class="form-group">
-                                    <label class="control-label col-md-4" for="mensaje">Contenido:</label>
-                                    <div class="col-sm-6">
-                                        <textarea class="form-control" rows="3" id="mensaje" name="mensaje" placeholder="Contenido del mensaje"></textarea>
-                                    </div>
-                                </div>
-                                <div class="form-group">
-                                    <div class="col-md-6 col-md-offset-4">
-                                        <button type="submit" class="btn btn-rbt">Enviar</button>
-                                    </div>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
+        <div class="row" id="contacto">
+            <div class="col-md-8 col-md-offset-2">
+                <h1>CONTACTO</h1>
+                <ul>
+                    @foreach($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+                <form class="form-horizontal " method="POST" action="contacto" >
+                    {{ csrf_field() }}
+                <div class="form-group">
+                    {!! Form::label('Nombre:') !!}
+                    {!! Form::text('nombre', null,
+                        array(
+                              'class'=>'form-control',
+                              'name'=>'nombre',
+                              'placeholder'=>'Introduce tu nombre')) !!}
                 </div>
+
+                <div class="form-group">
+                    {!! Form::label('E-mail:') !!}
+                    {!! Form::text('email', null,
+                        array(
+                              'class'=>'form-control',
+                              'name'=>'email',
+                              'placeholder'=>'Introduce tu correo')) !!}
+                </div>
+
+                <div class="form-group">
+                    {!! Form::label('Mensaje:') !!}
+                    {!! Form::textarea('mensaje', null,
+                        array('class'=>'form-control',
+                              'name'=>'mensaje',
+                              'placeholder'=>'Contenido del mensaje')) !!}
+                </div>
+
+                <div class="form-group">
+                    {!! Form::submit('Contactáctanos!',
+                      array('class'=>'btn btn-rbt')) !!}
+                </div>
+
+                </form>
             </div>
         </div>
     </div>
-
     {!! HTML::style('css/home.css') !!}
 @stop
