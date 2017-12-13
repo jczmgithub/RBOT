7dbba726b11e2dc5a31bb28ce2b9555e48397b16
Icampo <icampope16dw@ikzubirimanteo.com>
2017-12-13 10:37:25 +0100

    Usuario pagina

--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -23,7 +23,14 @@
             <div class="col-md-6">
                 <h3>Webcam</h3>
 
+                <div class="embed-responsive embed-responsive-16by9">
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/Y5Q_xjJQmBE"
+                            frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen>
+                    </iframe>
+                </div>
+
             </div>
+
             <div class="col-md-6">
 
                 <div class="tab-content">
