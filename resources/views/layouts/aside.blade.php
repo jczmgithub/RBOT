8e3ed39842817bc02f6db3ee1694629bb7e9dd8d
“Juan” <jcasinoste16dw@ikzubirimanteo.com>
2017-12-13 11:01:46 +0100

    aside

--- resources/views/layouts/aside.blade.php
+++ resources/views/layouts/aside.blade.php
@@ -1,4 +1,13 @@
 <aside>
 
+    <div id="mySidenav" class="sidenav">
+        <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+        <a href="#">About</a>
+        <a href="#">Services</a>
+        <a href="#">Clients</a>
+        <a href="#">Contact</a>
+    </div>
+
     {!! HTML::style('css/layouts/aside.css') !!}
+    {!! HTML::script('js/layouts/aside.js'); !!}
 </aside>
