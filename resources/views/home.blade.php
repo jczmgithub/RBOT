6706e76e8d95d848e5f922eb0de19685e8cb8d3b
xenlord <alopezca16dw@ikzubirimanteo.com>
2017-12-14 09:27:42 +0100

    Cambios landing page 2

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,7 +16,7 @@
     </div>
     <div id="seccionbrazo" class="container-fluid text-center">
         <div class="row">
-            <div class="col-lg-6">
+            <div class="col-lg-6 col-xs-12 col-sm-6">
 
                 <h1>Movilidad</h1>
                 <p>Maneja tu brazo robot desde cualquier lugar.</p>
@@ -31,7 +31,10 @@
                 <p>¡Disfruta de nustra aplicación web!</p>
 
             </div>
-            <iframe id="gifRobot" src="https://giphy.com/embed/6e5R2q1W3pA4g" frameBorder="0" class="img-responsive" allowFullScreen></iframe>
+            <video id="videoRobot" class="embed-responsive-item col-lg-6 col-xs-12 col-sm-6"  loop autoplay muted >
+                <source src="robotexample.mp4" type="video/mp4">
+                Your browser does not support the video tag.
+            </video>
         </div>
 
     </div>
@@ -69,61 +72,6 @@
             </div>
         </div>
     </div>
-
-    {{--<div id="contenedorContacto">--}}
-        {{--<div class="contenido container">--}}
-            {{--<div class="row" id="contacto">--}}
-                {{--<div class="col-md-8 col-md-offset-2">--}}
-                    {{--<h2 class="text-center">CONTACTO: </h2>--}}
-                            {{--@if ($errors->any())--}}
-                                {{--<div class="alert alert-danger">--}}
-                                    {{--<ul>--}}
-                                        {{--@foreach ($errors->all() as $error)--}}
-                                            {{--<div class="alert alert-danger">--}}
-                                                {{--<strong>Error! </strong> {{ $error }}--}}
-                                            {{--</div>--}}
-                                        {{--@endforeach--}}
-                                    {{--</ul>--}}
-                                {{--</div>--}}
-                            {{--@endif--}}
-                            {{--@if (session('status'))--}}
-                                {{--<div class="alert alert-success">--}}
-                                    {{--{{ session('status') }}--}}
-                                {{--</div>--}}
-                            {{--@endif--}}
-                            {{--<form class="form-horizontal" method="POST" action="contacto">--}}
-                                {{--{{ csrf_field() }}--}}
-
-                                {{--<div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">--}}
-                                    {{--<label class="control-label col-md-4" for="nombre">Nombre:</label>--}}
-                                    {{--<div class="col-md-6">--}}
-                                        {{--<input type="text" class="form-control" id="nombre" placeholder="Introduce tu nombre" name="nombre">--}}
-                                    {{--</div>--}}
-                                {{--</div>--}}
-
-                                {{--<div class="form-group">--}}
-                                    {{--<label class="control-label col-md-4" for="email">Email:</label>--}}
-                                    {{--<div class="col-sm-6">--}}
-                                        {{--<input type="email" class="form-control" id="email" placeholder="Introduce email" name="email">--}}
-                                    {{--</div>--}}
-                                {{--</div>--}}
-                                {{--<div class="form-group">--}}
-                                    {{--<label class="control-label col-md-4" for="mensaje">Contenido:</label>--}}
-                                    {{--<div class="col-sm-6">--}}
-                                        {{--<textarea class="form-control" rows="3" id="mensaje" name="mensaje" placeholder="Contenido del mensaje"></textarea>--}}
-                                    {{--</div>--}}
-                                {{--</div>--}}
-                                {{--<div class="form-group">--}}
-                                    {{--<div class="col-md-6 col-md-offset-4">--}}
-                                        {{--<button type="submit" class="btn btn-rbt">Enviar</button>--}}
-                                    {{--</div>--}}
-                                {{--</div>--}}
-                            {{--</form>--}}
-                        {{--</div>--}}
-            {{--</div>--}}
-        {{--</div>--}}
-    {{--</div>--}}
-
 
     <div id="contenedorContacto">
             <div class="col-md-8 col-md-offset-2 container">
