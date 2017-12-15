b0911f9c46e0e830e0f542ae26aa974f51a83b95
xenlord <alopezca16dw@ikzubirimanteo.com>
2017-12-15 10:17:43 +0100

    Cambios landing page 2

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,47 +3,46 @@
 @section('huecoNav')
     <li ><a href="#seccionbrazo">Características</a></li>
     <li ><a href="#servicios">Servicios</a></li>
-    <li ><a href="#contacto">Contacto</a></li>
+    <li ><a href="#contenedorContacto">Contacto</a></li>
 @stop
 
 @section('home')
 
-    <div class="container-fluid text-center" id="inicio">
-        <div id="descripcion">
-            <h1>eivneesnse</h1>
-        </div>
+    <div class="container-fluid text-center bordeAzul" id="inicio">
+        <h1 id="titulo" class="retroshadow zekton">RBOT<div id="descripcion">control service</div></h1>
+
+        <a href="{{ route('register') }}"><button type="button" class="btn-lg btn-rbt contenido">Probar Control</button></a>
+
     </div>
-
-
-    <div id="seccionbrazo" class="container-fluid text-center">
+    <div id="seccionbrazo" class="container-fluid text-center bordeAzul">
         <div class="row">
-            <div class="col-lg-6">
+            <div class="col-lg-6 col-xs-12 col-sm-6">
 
-                <h1>Movilidad</h1>
+                <h1 class="azul">Movilidad</h1>
                 <p>Maneja tu brazo robot desde cualquier lugar.</p>
 
-                <h1>Usabilidad</h1>
+                <h1 class="azul">Usabilidad</h1>
                 <p>Interfaz amigable para un máximo control.</p>
 
-                <h1>Eficiencia</h1>
+                <h1 class="azul">Eficiencia</h1>
                 <p>Ahorra tiempo y trabajo.</p>
 
-                <h1>User experience</h1>
+                <h1 class="azul">User experience</h1>
                 <p>¡Disfruta de nustra aplicación web!</p>
 
             </div>
-            <div class="col-lg-6">
-                <img style="margin: auto" src="/img/rbx4.png" class="img-responsive" alt="Mano robot">
-            </div>
+            <video id="videoRobot" class="embed-responsive-item col-lg-6 col-xs-12 col-sm-6"  loop autoplay muted >
+                <source src="robotexample.mp4" type="video/mp4">
+                Your browser does not support the video tag.
+            </video>
         </div>
 
     </div>
 
-    <div class="container" id="servicios">
+    <div class="bordeAzul" id="servicios">
         <div class="row">
             <div class="col-lg-12 text-center">
-                <h2 class="section-heading text-uppercase">Servicios</h2>
-                <h3 class="section-subheading text-muted">Servicios disponibles en RBOT.</h3>
+                <h1 class="section-heading text-uppercase">Servicios</h1>
             </div>
         </div>
         <div class="row text-center">
@@ -74,63 +73,45 @@
         </div>
     </div>
 
-    <div id="contenedorContacto">
-        <div class="contenido container">
-            <div class="row" id="contacto">
-                <div class="col-md-8 col-md-offset-2">
-                    <div class="panel panel-default">
-                        <div class="panel-heading"><h3>Contacto</h3></div>
-                        <div class="panel-body">
-                            @if ($errors->any())
-                                <div class="alert alert-danger">
-                                    <ul>
-                                        @foreach ($errors->all() as $error)
-                                            <div class="alert alert-danger">
-                                                <strong>Error! </strong> {{ $error }}
-                                            </div>
-                                        @endforeach
-                                    </ul>
-                                </div>
-                            @endif
-                            @if (session('status'))
-                                <div class="alert alert-success">
-                                    {{ session('status') }}
-                                </div>
-                            @endif
-                            <form class="form-horizontal" method="POST" action="contacto">
-                                {{ csrf_field() }}
-
-                                <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                                    <label class="control-label col-md-4" for="nombre">Nombre:</label>
-                                    <div class="col-md-6">
-                                        <input type="text" class="form-control" id="nombre" placeholder="Introduce tu nombre" name="nombre">
-                                    </div>
-                                </div>
-
-                                <div class="form-group">
-                                    <label class="control-label col-md-4" for="email">Email:</label>
-                                    <div class="col-sm-6">
-                                        <input type="email" class="form-control" id="email" placeholder="Introduce email" name="email">
-                                    </div>
-                                </div>
-                                <div class="form-group">
-                                    <label class="control-label col-md-4" for="mensaje">Contenido:</label>
-                                    <div class="col-sm-6">
-                                        <textarea class="form-control" rows="3" id="mensaje" name="mensaje" placeholder="Contenido del mensaje"></textarea>
-                                    </div>
-                                </div>
-                                <div class="form-group">
-                                    <div class="col-md-6 col-md-offset-4">
-                                        <button type="submit" class="btn btn-rbt">Enviar</button>
-                                    </div>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
+    <div id="contenedorContacto" class="text-center bordeAzul">
+        <div class="col-md-8 col-md-offset-2 container">
+            <h1>CONTACTO</h1>
+            <ul>
+                @foreach($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+            <form class="form-horizontal" method="POST" action="contacto" >
+                {{ csrf_field() }}
+                <div class="form-group">
+                    {!! Form::label('Nombre:') !!}
+                    {!! Form::text('nombre', null,
+                        array(
+                              'class'=>'form-control',
+                              'name'=>'nombre',
+                              'placeholder'=>'Introduce tu nombre')) !!}
                 </div>
-            </div>
+                <div class="form-group">
+                    {!! Form::label('E-mail:') !!}
+                    {!! Form::text('email', null,
+                        array(
+                              'class'=>'form-control',
+                              'name'=>'email',
+                              'placeholder'=>'Introduce tu correo')) !!}
+                </div>
+                <div class="form-group">
+                    {!! Form::label('Mensaje:') !!}
+                    {!! Form::textarea('mensaje', null,
+                        array('class'=>'form-control',
+                              'name'=>'mensaje',
+                              'placeholder'=>'Contenido del mensaje')) !!}
+                </div>
+                <div class="form-group">
+                    {!! Form::submit('Contactáctanos!',
+                      array('class'=>'btn btn-rbt')) !!}
+                </div>
+            </form>
         </div>
     </div>
-
     {!! HTML::style('css/home.css') !!}
 @stop
