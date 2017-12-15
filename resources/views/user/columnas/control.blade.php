00d2bfd3c3390c59d932095efbe5d365af09a8e4
Icampo <icampope16dw@ikzubirimanteo.com>
2017-12-15 11:03:18 +0100

    layout user.columnas

---
+++ resources/views/user/columnas/control.blade.php
@@ -0,0 +1,36 @@
+<div class="col-sm-4">
+
+    <h3>Controles</h3>
+
+    <div id="controles">
+
+        <form action="/">
+
+            <select class="form-control selector">
+
+                <option value="">--Seleccione el Robot--</option>
+                <option value="robot1">Robot1</option>
+                <option value="robot2">Robot2</option>
+                <option value="robot3">Robot3</option>
+
+            </select>
+
+            <select class="form-control selector" id="selecMotor">
+
+                <option value="">--Seleccione el motor--</option>
+                <option value="motor1">Base</option>
+                <option value="motor2">Hombro</option>
+                <option value="motor3">Brazo</option>
+                <option value="motor4">Codo</option>
+                <option value="motor5">Muñeca</option>
+                <option value="motor6">Mano</option>
+
+            </select>
+
+            <h5>Velocidad:</h5>
+
+        </form>
+
+    </div>
+
+</div>
---
+++ resources/views/user/columnas/robot.blade.php
@@ -0,0 +1,5 @@
+<div class="col-sm-4">
+
+    <h3>Robot</h3>
+
+</div>
