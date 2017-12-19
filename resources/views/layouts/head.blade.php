91d44d057b5d96082464560baa32ba8cde97236d
“Juan” <jcasinoste16dw@ikzubirimanteo.com>
2017-12-19 10:53:20 +0100

    prueba bootstrap4

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -3,12 +3,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <title>RBOT</title>
-    <!-- load bootstrap  -->
-    <link rel="stylesheet" href="/css/bootstrap.css">
+    <!-- load bootstrap
+    <link rel="stylesheet" href="/css/bootstrap.css">-->
+    <!-- Latest compiled JavaScript
+    <script src="/js/bootstrap.js"></script>-->
+    <!-- load bootstrap 4  -->
+    <link rel="stylesheet" href="/css/bootstrap4.css">
+    <!-- Latest compiled JavaScript 4-->
+    <script src="/js/bootstrap4.js"></script>
     <!-- jQuery library -->
     <script src="/js/jquery-3.2.1.min.js"></script>
-    <!-- Latest compiled JavaScript -->
-    <script src="/js/bootstrap.js"></script>
     <!-- mi css -->
     <link rel="stylesheet" href="/css/rbot.css">
     <!-- Glyphicons -->
