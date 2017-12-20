ab2fce97350591f84ea1c469e36f4ee368ce8e96
“Juan” <jcasinoste16dw@ikzubirimanteo.com>
2017-12-20 09:18:37 +0100

    arreglar error 404

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,4 +1,4 @@
-@extends('landing.layouts.default')
+@extends('layouts.default')
 @section('home')
     <iframe class="container center-block" src="https://giphy.com/embed/VwoJkTfZAUBSU" width="332" height="480" frameBorder="0" ></iframe>
     <h1 class="text-center">Página en construcción</h1>
