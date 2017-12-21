081f7a4a75786e51e95ff85072ebeed663883823
“Juan” <jcasinoste16dw@ikzubirimanteo.com>
2017-12-21 10:24:02 +0100

    tabla usuarios

--- resources/views/admin/tabs/modificarUsuario.blade.php
+++ resources/views/admin/tabs/modificarUsuario.blade.php
@@ -1,20 +1,38 @@
 <div id="ModificarUsu" class="tabcontent">
 
-    <div>
-        <table>
-            <tr>
-                <th>Nombre</th>
-                <th>Email</th>
-            </tr>
-        </table>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <table id="tablaUsuarios">
+                    <tr>
+                        <th>Nombre</th>
+                        <th>Email</th>
+                        <th>Editar</th>
+                        <th>Eliminar</th>
+                    </tr>
+
+                    @foreach($users as $key => $data)
+                        <tr>
+                            <td>
+                                {{ $data->name }}
+                            </td>
+                            <td>
+                                {{ $data->email }}
+                            </td>
+                            <td>
+                                <button type="button">Editar</button>
+                            </td>
+                            <td>
+                                <button type="button">Eliminar</button>
+                            </td>
+                        </tr>
+                    @endforeach
+
+                </table>
+            </div>
+        </div>
     </div>
 
-    @foreach($users as $key => $data)
-        <p>
-            nombre: {{ $data->name }}
-            <br>
-            email: {{ $data->email }}
-        </p>
-    @endforeach
+
 
 </div>
