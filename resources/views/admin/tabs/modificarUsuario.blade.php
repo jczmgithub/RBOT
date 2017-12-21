4605c560769bdeba293373af572b4184ed0ba415
“Juan” <jcasinoste16dw@ikzubirimanteo.com>
2017-12-21 08:28:20 +0100

    tabla usuarios

--- resources/views/admin/tabs/modificarUsuario.blade.php
+++ resources/views/admin/tabs/modificarUsuario.blade.php
@@ -1,5 +1,14 @@
 <div id="ModificarUsu" class="tabcontent">
 
+    <div>
+        <table>
+            <tr>
+                <th>Nombre</th>
+                <th>Email</th>
+            </tr>
+        </table>
+    </div>
+
     @foreach($users as $key => $data)
         <p>
             nombre: {{ $data->name }}
