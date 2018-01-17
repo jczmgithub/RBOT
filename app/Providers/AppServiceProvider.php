2e60545f8382d79fd62f230cf5362c868160e79f
“Juan” <jcasinoste16dw@ikzubirimanteo.com>
2018-01-17 13:05:28 +0100

    pruebas error formularios login heroku

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,6 +26,6 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         //
-        $this->app['request']->server->set('HTTPS', true);
+        //$this->app['request']->server->set('HTTPS', true);
     }
 }
