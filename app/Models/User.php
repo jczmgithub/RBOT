57ca1b2dbfaeba4f33822fc3812ca09482a48949
“Juan” <jcasinoste16dw@ikzubirimanteo.com>
2018-01-23 12:24:05 +0100

    arreglar merge de aitor!!!, User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -26,7 +26,7 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token', 'credito'
     ];
-    
+
     public function robots() {
         return $this->hasMany('App\Models\Robot');
     }
