1c76d2f6467543bb68ae5f7ff11e5e8055f41e2a
Icampo <icampope16dw@ikzubirimanteo.com>
2018-02-02 10:38:36 +0100

    añadir modal para cargar archivos directorio ftp

---
+++ resources/views/user/includes/secuencias.blade.php
@@ -0,0 +1,25 @@
+<div class="modal fade" id="myModal" role="dialog">
+    <div class="modal-dialog">
+
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <h4 class="modal-title">Secuencias</h4>
+            </div>
+            <div class="modal-body">
+                <p>Selecciona la Secuencia que desea ejecutar:</p>
+                <p>
+
+                        <select name='selecSecuencia' class='form-control selector totalWidth' id="selecSecuencia">
+                            <option>--Elige Secuencia--</option>
+                        </select>
+                </p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" id="cargarDatos" class="btn btn-primary" data-dismiss="modal">Abrir</button>
+                <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+            </div>
+        </div>
+
+    </div>
+</div>
