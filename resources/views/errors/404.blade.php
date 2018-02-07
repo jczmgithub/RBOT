2b2dc233b1c83c276341cd98552c7bad7f5b776d
“Juan” <jcasinoste16dw@ikzubirimanteo.com>
2018-02-07 14:01:23 +0100

    error 404

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.default')
 @section('home')
-    <iframe class="container center-block" src="https://giphy.com/embed/VwoJkTfZAUBSU" width="332" height="480" frameBorder="0" ></iframe>
+    {{--<iframe class="container center-block" src="https://giphy.com/embed/VwoJkTfZAUBSU" width="332" height="480" frameBorder="0" ></iframe>--}}
     <h1 class="text-center">Página en construcción</h1>
+    <img style="margin: auto; margin-bottom: 5vh" src="img/404.gif" class="img-responsive" alt="Prueba">
 @stop
